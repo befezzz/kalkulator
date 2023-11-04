bbe422b9c6c1276e3605f081f245ce4f9260e84f
befezzz <92450114+befezzz@users.noreply.github.com>
2023-11-04 12:00:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ class Calculator{
             case '+':
                 computation = prev + current
                 break
-            case '':
+            case '-':
                 computation = prev - current
                 break
             case '*':
